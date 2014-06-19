21838d01817777544927c368064185fe068612e4
Andrey Antukh <niwi@niwi.be>
2014-06-19 02:16:23 +0200

    Update build docs script.

--- build-docs.sh
+++ build-docs.sh
@@ -1,31 +1,14 @@
 #!/bin/sh
-lein doc
 (cd doc; make)
+cp -vr doc/index.html /tmp/index.html;
+cp -vr doc/static /tmp/static
+git checkout gh-pages;
 
-if [ $1 == "devel" ]; then
-    echo "Building devel docs"
-    sleep 3
-    cp -vr doc/index.html /tmp/index.html;
-    git checkout gh-pages;
-    mv -fv /tmp/index.html index-dev.html
-    git add --all index-dev.html
-    git commit -a -m "Update devel doc"
-else
-    echo "Build stable docs"
-    sleep 3
-    cp -vr doc/index.html /tmp/index.html;
-    cp -vr doc/static /tmp/static
-    cp -vr doc/api /tmp/api
-    git checkout gh-pages;
+rm -rf static index.html
+mv -fv /tmp/static .
+mv -fv /tmp/index.html .
 
-    rm -rf *
-    mv -fv /tmp/static .
-    mv -fv /tmp/index.html .
-    mv -fv /tmp/api .
+git add --all index.html
+git add --all static
 
-    git add --all index.html
-    git add --all api
-    git add --all static
-
-    git commit -a -m "Update doc"
-fi
+git commit -a -m "Update doc"
