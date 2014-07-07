e5fa2d43c59228edcfe1ffc1b9179935094fb982
Andrey Antukh <niwi@niwi.be>
2014-07-07 20:40:57 +0200

    Minor refactor on project.clj

--- project.clj
+++ project.clj
@@ -3,13 +3,28 @@
   :url "https://github.com/niwibe/cats"
   :license {:name "BSD (2 Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2227"]]
+
   :source-paths ["target/classes"]
   :test-paths ["target/testclasses"]
   :deploy-repositories {"releases" :clojars
                         "snapshots" :clojars}
   :release-tasks [["cljx" "once"]
                   ["deploy" "clojars"]]
+
+  :plugins [[com.cemerick/clojurescript.test "0.3.1"]
+            [com.keminglabs/cljx "0.4.0" :exclusions [org.clojure/clojure]]
+            [lein-cljsbuild "1.0.3"]
+            [codox "0.8.9"]]
+
+  :hooks [cljx.hooks]
+
+  :codox {:sources ["target/classes"]
+          :output-dir "doc/codox"}
+
+  :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
   :cljsbuild {:test-commands {"unit-tests" ["node" :node-runner
                                             "target/tests.js"]}
               :builds {:test {:source-paths ["target/testclasses"
@@ -17,28 +32,23 @@
                               :compiler {:output-to "target/tests.js"
                                          :optimizations :simple
                                          :pretty-print true}}}}
+
+  :cljx {:builds [{:source-paths ["src/cljx"]
+                   :output-path "target/classes"
+                   :rules :clj}
+                  {:source-paths ["src/cljx"]
+                   :output-path "target/classes"
+                   :rules :cljs}
+                  {:source-paths ["tests"]
+                   :output-path "target/testclasses"
+                   :rules :clj}
+                  {:source-paths ["tests"]
+                   :output-path "target/testclasses"
+                   :rules :cljs}]}
   :profiles
-  {:common {:hooks [cljx.hooks]
-            :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
-            :dependencies [[org.clojure/clojurescript "0.0-2227"]
-                           [org.clojure/tools.namespace "0.2.4"]]
-            :plugins [[com.cemerick/clojurescript.test "0.3.1"]
-                      [com.keminglabs/cljx "0.4.0" :exclusions [org.clojure/clojure]]
-                      [lein-cljsbuild "1.0.3"]
-                      [codox "0.8.9"]]
-            :codox {:sources ["target/classes"]
-                    :output-dir "doc/codox"}
-            :cljx {:builds [{:source-paths ["src/cljx"]
-                             :output-path "target/classes"
-                             :rules :clj}
-                            {:source-paths ["src/cljx"]
-                             :output-path "target/classes"
-                             :rules :cljs}
-                            {:source-paths ["tests"]
-                             :output-path "target/testclasses"
-                             :rules :clj}
-                            {:source-paths ["tests"]
-                             :output-path "target/testclasses"
-                             :rules :cljs}]}}
-   :dev [:common {:source-paths ["dev"]}]
-   :test [:common {:hooks [cljx.hooks leiningen.cljsbuild]}]})
+  {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
+         ;; Disabled, bacause the user.clj try
+         ;; import clojure namespaces before create
+         ;; them (ex: after removing target/ directory)
+         ;; :source-paths ["dev"]
+         }})
