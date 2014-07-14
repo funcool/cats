bf4524916d5602f6fc820b8dd7b7da23875a3c60
Alejandro Gómez <alejandro@dialelo.com>
2014-07-14 22:40:50 +0200

    better user.test

--- dev/user.clj
+++ dev/user.clj
@@ -1,3 +1,16 @@
 (ns user
   (:require [clojure.tools.namespace.repl :refer [refresh]]
-            [cats.core :as m]))
+            [clojure.test :refer [run-tests]])
+  (:refer-clojure :exclude [test]))
+
+(defn test
+  ([]
+   (refresh)
+   (run-tests 'test-core
+              'test-builtin
+              'test-maybe
+              'test-either
+              'test-state
+              'test-continuation))
+  ([& namespaces]
+   (apply run-tests namespaces)))
