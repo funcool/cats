763e79f69cd29024b0f15e33633fae510b459963
Andrey Antukh <niwi@niwi.be>
2014-10-18 20:47:51 +0200

    Update the dev build docs script.

--- build-devdocs.sh
+++ build-devdocs.sh
@@ -2,18 +2,17 @@
 lein doc
 (cd doc; make)
 cp -vr doc/index.html /tmp/index.html;
-cp -vr doc/static /tmp/static
 cp -vr doc/codox /tmp/codox
-cp -vr logo /tmp/static
+cp -vr logo/logo.png /tmp/cats-logo.png
 git checkout gh-pages;
 
-rm -rf static index-dev.html codox-dev
-mv -fv /tmp/static .
+rm -rf index-dev.html codox-dev
+mv -fv /tmp/cats-logo.png logo.png
 mv -fv /tmp/index.html ./index-dev.html
 mv -fv /tmp/codox ./codox-dev
 
 git add --all index-dev.html
-git add --all static
 git add --all codox-dev
+git add --all logo.png
 
 git commit -a -m "Update dev doc"
