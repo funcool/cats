5ef43d410f88aa2d83cc34e04642fb8897e5e3c4
Andrey Antukh <niwi@niwi.be>
2014-10-26 13:42:30 +0100

    Update docs build script.

--- build-docs.sh
+++ build-docs.sh
@@ -2,18 +2,17 @@
 lein doc
 (cd doc; make)
 cp -vr doc/index.html /tmp/index.html;
-cp -vr doc/static /tmp/static
 cp -vr doc/codox /tmp/codox
-cp -vr logo /tmp/static
+cp -vr logo/logo.png /tmp/cats-logo.png
 git checkout gh-pages;
 
-rm -rf static index.html codox
-mv -fv /tmp/static .
-mv -fv /tmp/index.html .
-mv -fv /tmp/codox .
+rm -rf index.html codox
+mv -fv /tmp/cats-logo.png logo.png
+mv -fv /tmp/index.html ./index.html
+mv -fv /tmp/codox ./codox
 
 git add --all index.html
-git add --all static
 git add --all codox
+git add --all logo.png
 
-git commit -a -m "Update doc"
+git commit -a -m "Update dev doc"
