b1ebf368788351f1613dcece53e02ef7320146f6
Andrey Antukh <niwi@niwi.be>
2015-02-21 12:08:53 +0100

    Set 0.4.0-SNAPSHOT version on project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cats "0.3.0-SNAPSHOT"
+(defproject cats "0.4.0-SNAPSHOT"
   :description "Category Theory abstractions for Clojure"
   :url "https://github.com/funcool/cats"
   :license {:name "BSD (2 Clause)"
