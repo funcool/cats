27ad470f9bb2e2ab75204a0157b7643ffb4095e5
Andrey Antukh <niwi@niwi.be>
2015-02-28 01:26:15 +0100

    Remove deprecated test/run.js file.

--- test/run.js
+++
@@ -1,9 +0,0 @@
-try {
-    require("source-map-support").install();
-} catch(err) {
-}
-require("../output/out/goog/bootstrap/nodejs.js");
-require("../output/tests.js");
-
-goog.require("cats.testrunner");
-goog.require("cljs.nodejscli");
