758fca8ac9a88b2d4a13c6213778d8259c1dcf43
Andrey Antukh <niwi@niwi.be>
2015-03-22 00:38:53 +0100

    Add missing dependency entry.

--- project.clj
+++ project.clj
@@ -42,7 +42,8 @@
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]
                                   [org.clojure/clojure "1.6.0"]
                                   [org.clojure/clojurescript "0.0-3126"]
-                                  [funcool/cljs-testrunners "0.1.0-SNAPSHOT"]]
+                                  [funcool/cljs-testrunners "0.1.0-SNAPSHOT"]
+                                  [funcool/codeina "0.1.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]
                    :codeina {:sources ["output/src"]
                              :output-dir "doc/codeina"}
                    :plugins [[org.clojars.cemerick/cljx "0.6.0-SNAPSHOT"
