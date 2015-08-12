6cca752d92fe31609eef20612f50dd02d6113014
Andrey Antukh <niwi@niwi.nz>
2015-08-12 10:19:18 +0300

    Move profiles declaration to profiles.clj (and set version to 0.7.0-SNAPSHOT)

---
+++ profiles.clj
@@ -0,0 +1,15 @@
+{:dev
+ {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
+  :source-paths ["dev"]
+  :codeina {:sources ["src"]
+            :reader :clojure
+            :target "doc/dist/latest/api"
+            :src-uri "http://github.com/funcool/cats/blob/master/"
+            :src-uri-prefix "#L"}
+  :plugins [[funcool/codeina "0.3.0-SNAPSHOT"]
+            [lein-ancient "0.6.7" :exclusions [org.clojure/tools.reader]]]}
+ :bench
+ [:dev
+  {:dependencies [[criterium "0.4.3"]]
+   :main ^:skip-aot benchmarks
+   :jvm-opts ^:replace []}]}
--- project.clj
+++ project.clj
@@ -1,23 +1,12 @@
-(defproject funcool/cats "0.6.1"
+(defproject funcool/cats "0.7.0-SNAPSHOT"
   :description "Category Theory abstractions for Clojure"
   :url "https://github.com/funcool/cats"
   :license {:name "BSD (2 Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.7.28" :scope "provided"]]
+                 [org.clojure/clojurescript "1.7.107" :scope "provided"]]
   :deploy-repositories {"releases" :clojars
                         "snapshots" :clojars}
   :source-paths ["src"]
   :test-paths ["test"]
-  :jar-exclusions [#"\.swp|\.swo"]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
-                   :codeina {:sources ["src"]
-                             :reader :clojure
-                             :target "doc/dist/latest/api"
-                             :src-uri "http://github.com/funcool/cats/blob/master/"
-                             :src-uri-prefix "#L"}
-                   :plugins [[funcool/codeina "0.3.0-SNAPSHOT"]]}
-             :bench [:dev {:dependencies [[criterium "0.4.3"]]
-                           :main ^:skip-aot benchmarks
-                           :jvm-opts ^:replace []
-                           :source-paths ["dev"]}]})
+  :jar-exclusions [#"\.swp|\.swo|user\.clj"])
