b127185e564f440f34af778bd6079a64629fa758
Andrey Antukh <niwi@niwi.nz>
2015-09-18 00:18:05 +0300

    Revert "Upgrade lein before run tests."
    This reverts commit beb988c77e1c120b730202f07203add2be9544d7.

--- .travis/test-clj.sh
+++ .travis/test-clj.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-lein2 upgrade
-lein2 test
+# FIXME: Explicit namespace are needed because Leiningen ignores .cljc files :_
+#  https://github.com/technomancy/leiningen/issues/1827
+TEST_NAMESPACES=(
+    cats.core-spec
+    cats.builtin-spec
+    cats.applicative.validation-spec
+    cats.monad.exception-spec
+    cats.monad.either-spec
+    cats.monad.maybe-spec
+    cats.monad.identity-spec
+)
+
+lein2 test ${TEST_NAMESPACES[@]}
