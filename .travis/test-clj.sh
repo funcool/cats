beb988c77e1c120b730202f07203add2be9544d7
Andrey Antukh <niwi@niwi.nz>
2015-09-18 00:10:08 +0300

    Upgrade lein before run tests.

--- .travis/test-clj.sh
+++ .travis/test-clj.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-# FIXME: Explicit namespace are needed because Leiningen ignores .cljc files :_
-#  https://github.com/technomancy/leiningen/issues/1827
-TEST_NAMESPACES=(
-    cats.core-spec
-    cats.builtin-spec
-    cats.applicative.validation-spec
-    cats.monad.exception-spec
-    cats.monad.either-spec
-    cats.monad.maybe-spec
-    cats.monad.identity-spec
-)
-
-lein2 test ${TEST_NAMESPACES[@]}
+lein2 upgrade
+lein2 test
