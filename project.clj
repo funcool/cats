5575d7b72bc43d777bc79bdd06994a1fc8354ae8
Andrey Antukh <niwi@niwi.nz>
2015-11-05 16:50:38 +0200

    Update cljs compiler to 1.7.145

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "BSD (2 Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.7.107" :scope "provided"]
+                 [org.clojure/clojurescript "1.7.145" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha" :scope "provided"]
                  [manifold "0.1.0" :scope "provided"]]
   :deploy-repositories {"releases" :clojars
