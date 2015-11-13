f8855cdb79455b3bb50f778f7a9c43df88bc4e17
Eric Bailey <eric@ericb.me>
2015-11-12 18:54:01 -0600

    Upgrade core.async

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url  "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [org.clojure/clojurescript "1.7.170" :scope "provided"]
-                 [org.clojure/core.async "0.2.371" :scope "provided"]
+                 [org.clojure/core.async "0.2.374" :scope "provided"]
                  [org.clojure/test.check "0.8.2" :scope "provided"]
                  [manifold "0.1.1" :scope "provided"]]
   :deploy-repositories {"releases"  :clojars
