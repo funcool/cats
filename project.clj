c8f00724a31d6613fbf208579ec91d5b29c6b2f7
Andrey Antukh <niwi@niwi.nz>
2015-11-25 22:12:23 +0200

    Update core.async and test.check dependencies.

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [org.clojure/clojurescript "1.7.170" :scope "provided"]
-                 [org.clojure/core.async "0.2.371" :scope "provided"]
-                 [org.clojure/test.check "0.8.2" :scope "provided"]
+                 [org.clojure/core.async "0.2.374" :scope "provided"]
+                 [org.clojure/test.check "0.9.0" :scope "provided"]
                  [manifold "0.1.1" :scope "provided"]]
   :deploy-repositories {"releases" :clojars
                         "snapshots" :clojars}
