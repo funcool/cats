2d4cd133784000163b898b223ab1c7de92694220
Atamert Ölçgen <muhuk@muhuk.com>
2016-02-13 10:55:58 +0200

    Changed ClojureScript optimization level to :simple.

--- scripts/build.clj
+++ scripts/build.clj
@@ -8,7 +8,7 @@
    {:main 'cats.runner
     :output-to "out/tests.js"
     :output-dir "out"
-    :optimizations :advanced
+    :optimizations :simple
     :target :nodejs
     :verbose true})
   (println "... done. Elapsed" (/ (- (System/nanoTime) start) 1e9) "seconds"))
