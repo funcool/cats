d378310f3cb6d007b85bebc848ea28a990b4906e
ericvm <ericvm@users.noreply.github.com>
2017-07-17 16:49:22 -0300

    Add s3-wagon, repositories, bump version (#1)

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject funcool/cats "2.1.0"
+(defproject funcool/cats "2.2.0"
   :description "Category Theory abstractions for Clojure"
   :url         "https://github.com/funcool/cats"
   :license {:name "BSD (2 Clause)"
             :url  "http://opensource.org/licenses/BSD-2-Clause"}
+  
+  :plugins [[s3-wagon-private "1.3.0"]]
+  
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/clojurescript "1.9.89" :scope "provided"]
                  [org.clojure/core.async "0.3.442" :scope "provided"]
@@ -10,8 +13,9 @@
                  [org.clojure/core.match "0.3.0-alpha4" :scope "provided"]
                  [manifold "0.1.5" :scope "provided"]
                  [funcool/promesa "1.8.1" :scope "provided"]]
-  :deploy-repositories {"releases"  :clojars
-                        "snapshots" :clojars}
+  :repositories  [["central"  {:url "http://repo1.maven.org/maven2/" :snapshots false}]
+                  ["clojars"  {:url "https://clojars.org/repo/"}]
+                  ["nu-maven" {:url "s3p://nu-maven/releases/"}]]
   :source-paths   ["src"]
   :test-paths     ["test"]
   :jar-exclusions [#"\.swp|\.swo|user\.clj"])
